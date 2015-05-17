5d638e81660039f421a6b7ebda04a9dbac147e83
Peter Gabrielsen <peterg@cs.au.dk>
2015-05-17 05:46:39 -0700

    even better fits

--- plot_given_test_dat_running.sh
+++ plot_given_test_dat_running.sh
@@ -14,15 +14,15 @@ set xr [0:1100]
 set yr [0:50]
 set key right top
 
-f(x) = a*x**b-x**c;
-g(x) = (m*x**n)*(log(x)/log(2))
-fit f(x) 'data/floyd_test.dat' using ($1):($2 / (3*($1**2))) via a,b,c
-fit g(x) 'data/sankowski_test.dat' using ($1):($2 / (3*($1**2))) via m,n
+f(x) = a*x**3
+g(x) = (m*x**2)*(log(x)/log(2))
+fit f(x) 'data/floyd_test.dat' using ($1):($2 / (3*($1**2))) via a
+fit g(x) 'data/sankowski_test.dat' using ($1):($2 / (3*($1**2))) via m
 
 plot "data/floyd_test.dat" using 1:($2 / (3*($1**2))) title 'Floyd-Warshall' pointtype 5,\
      "data/sankowski_test.dat" using 1:($2 / (3*($1**2))) title 'Sankowski' pointtype 7,\
-     f(x) with lines title sprintf("Floyd-fit %.5fx^{%.5f}-x^{%.5f}", a,b,c),\
-     g(x) with lines title sprintf("Sankowski-fit %.5fx^{%.5f} log_2(x)", m,n)
+     f(x) with lines title sprintf("Floyd-fit %.3Ex^{%.1f}", a,3,0),\
+     g(x) with lines title sprintf("Sankowski-fit %.3Ex^{%.1f} log_2(x)", m,2)
 
 
 
